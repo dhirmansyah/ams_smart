00071a8a4147c70727ca5bc4095ae1524cb1676d
root <hirmansyah.doni@gmail.com>
2018-10-31 13:31:07 +0700

    mantap2

--- test.py
+++ test.py
@@ -6,30 +6,67 @@ import sys
 import random
 from pandas import DataFrame
 
+
+
 datenow=(time.strftime("%Y-%m-%d"))
 filename=sys.argv[1]
 ADEX_FILE='/opt/output/adexchange.csv'
+ZM_FILE='/opt/output/adzimbra.csv'
 
-#df = pd.read_csv(filename)
-df = pd.read_csv(filename, usecols=['EMP_NO','USERNAME','FIRST_NAME','LAST_NAME','Email','AD','EXCHANGE','MANAGER','POSITION','WORK_LOCATION','SUPERVISOR','PASSWORD'], sep=',|;', engine='python')
+#df = pd.read_csv(filename)df = pd.read_csv(filename, usecols=['EMP_NO','USERNAME','FIRST_NAME','LAST_NAME','EMAIL','AD','EXCHANGE','MANAGER','POSITION','CITY','SUPERVISOR','PASSWORD'], sep=',|;', engine='python')
+df = pd.read_csv(filename, usecols=['EMP_NO','USERNAME','FIRST_NAME','LAST_NAME','EMAIL','AD','EXCHANGE','MANAGER','POSITION','CITY','SUPERVISOR','PASSWORD'], sep=',|;', engine='python')
+## Split email address to sAMAccountName and limit just 20 Char in there because of AD limitation
+sAMAccountName_field = df.EMAIL.str.split("@").str[0].str.slice(0, 20)
+df['sAMAccountName'] = sAMAccountName_field.str.slice(0, 20)
+#print(sAMAccountName_field)
+
+#Add CN column
+cn_field = df.FIRST_NAME +" "+ df.LAST_NAME
+df['cn'] = cn_field
+#print (cn_field)
+
+#Add date Created column
+date_field = datenow
+df['date_created'] = date_field
 
 
 
-# Spesify Field by feature and Position
-adexchange = df.loc[(df.AD == 'YES') & (df.EXCHANGE == 'YES')]
-adzimbra = df.loc[(df.AD == 'YES') & (df.EXCHANGE == 'NO')]
-zimbraonly = df.loc[(df.AD == 'NO') & (df.EXCHANGE == 'NO')]
+
+# DECIDE Field by feature and Position
+AD_EXCHANGE = df.loc[(df.AD.str.lower() == 'yes') & (df.EXCHANGE.str.lower() == 'yes')]
+AD_ZIMBRA = df.loc[(df.AD.str.lower() == 'yes') & (df.EXCHANGE.str.lower() == 'no')]
+zimbraonly = df.loc[(df.AD.str.lower() == 'no') & (df.EXCHANGE.str.lower() == 'no')]
+
 
 
 # Printing based on value 1 before export
 print ("-----------------------Exchange only--------------------------------")
-print(adexchange.astype('str').replace('\.0', '', regex=True))
+print(AD_EXCHANGE.astype('str').replace('\.0', '', regex=True))
+print
+print
 print ("-----------------------AD Zimbra--------------------------------")
-print(adzimbra.astype('str').replace('\.0', '', regex=True))
-print ("-----------------------Zimbra only--------------------------------")
-print(zimbraonly.astype('str').replace('\.0', '', regex=True))
+print(AD_ZIMBRA.astype('str').replace('\.0', '', regex=True))
+print
+print
+#print ("-----------------------Zimbra only--------------------------------")
+#print(zimbraonly.astype('str').replace('\.0', '', regex=True))
+
+
+
+#Global Table AD Format
+#tbl_adex=['employe_number','username','first_name','last_name','email','position','l','supervisior','manager','enable_mailbox','SmAccount']
+tbl_adex=['employe_number','username','first_name','last_name','email','position','l','supervisior','manager','enable_mailbox','sAMAccountName','cn']
 
 # Export to csv ADEX
-exportadexchange = adexchange.astype('str').replace('\.0', '', regex=True)
-tbl_adex=['employe_number','employe_number','employe_number','employe_number','employe_number','employe_number','employe_number','employe_number','employe_number']
-exportadexchange.to_csv(ADEX_FILE, sep=',', encoding='utf-8', index = None, header=tbl_adex,columns=['EMP_NO','USERNAME','FIRST_NAME','LAST_NAME','Email','MANAGER','POSITION','WORK_LOCATION','SUPERVISOR'])
+CONV_AD_EXCHANGE = AD_EXCHANGE.astype('str').replace('\.0', '', regex=True)
+CONV_AD_EXCHANGE.to_csv(ADEX_FILE, sep=',', encoding='utf-8', index = None, header=tbl_adex,columns=['EMP_NO','USERNAME','FIRST_NAME','LAST_NAME','EMAIL','POSITION','CITY','SUPERVISOR','MANAGER','EXCHANGE','sAMAccountName','cn'])
+
+# EXPORT TO CSV FOR AD_ONLY
+CONV_AD_ONLY = AD_ZIMBRA.astype('str').replace('\.0', '', regex=True)
+CONV_AD_ONLY.to_csv(ADEX_FILE, mode = 'a' ,sep=',', encoding='utf-8', index = None, header=None,columns=['EMP_NO','USERNAME','FIRST_NAME','LAST_NAME','EMAIL','POSITION','CITY','SUPERVISOR','MANAGER','EXCHANGE','sAMAccountName','cn'])
+
+
+
+
+# EXPORT to csv ad-zimbra
+
