2538045101bbeca79756ef1e02e4c4cd4bfd3e85
root <hirmansyah.doni@gmail.com>
2018-10-31 07:07:18 +0700

    ketemu solusi parsing, tinggal restructure based on requirement sistem seperti AD

--- test.py
+++ test.py
@@ -10,24 +10,20 @@ datenow=(time.strftime("%Y-%m-%d"))
 filename=sys.argv[1]
 
 #df = pd.read_csv(filename)
-df = pd.read_csv(filename, usecols=['EMP_NO','USERNAME','FIRST_NAME','LAST_NAME','Email','AD','EXCHANGE','MANAGER','POSITION','WORK_LOCATION'], sep=';')
-#df = pd.read_csv(filename)
-#df.loc[(df['AD']=="YES") & (df['EXCHANGE']=="YES"),['EMP_NO','USERNAME','Email']]
-#df = pd.read_csv(filename, sep=';')
-#df = pd.read_csv(filename,names =['EMP_NO','USERNAME','Email','AD','EXCHANGE','MANAGER'], sep=';' )
-#df = pd.read_csv(filename,names =['MANAGER'], sep=';' )
+df = pd.read_csv(filename, usecols=['EMP_NO','USERNAME','FIRST_NAME','LAST_NAME','Email','AD','EXCHANGE','MANAGER','POSITION','WORK_LOCATION','SUPERVISOR'], sep=',|;', engine='python')
 
-#data['EMP_NO'] = [random.randint(0,1000) for x in range(data.shape[0])]
-#df = pd.DataFrame(data,)
 
-#Replace header colom 
-#df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
- 
-#data.head(5)
-#print(df)
-#print(df["EMP_NO"],df["MANAGER"].astype('str').replace('\.0', '', regex=True))
-ADEXCHANGE = (df["AD"]=="YES") & (df['EXCHANGE']=="YES")
-#df.loc[df.AD == "YES"]
-#print(pd.notnull(df["MANAGER"]))
-#df.head(4)
-print(df)
+
+# Spesify Field by feature and Position
+adexchange = df.loc[(df.AD == 'YES') & (df.EXCHANGE == 'YES')]
+adzimbra = df.loc[(df.AD == 'YES') & (df.EXCHANGE == 'NO')]
+zimbraonly = df.loc[(df.AD == 'NO') & (df.EXCHANGE == 'NO')]
+
+
+# Printing based on value
+print ("-----------------------Exchange only--------------------------------")
+print(adexchange.astype('str').replace('\.0', '', regex=True))
+print ("-----------------------AD Zimbra--------------------------------")
+print(adzimbra.astype('str').replace('\.0', '', regex=True))
+print ("-----------------------Zimbra only--------------------------------")
+print(zimbraonly.astype('str').replace('\.0', '', regex=True))
