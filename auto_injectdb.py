6b234fc07ff4172faeff024b7dfcab22fc8310b6
root <hirmansyah.doni@gmail.com>
2018-11-01 11:29:10 +0700

    berhasil inject database

--- auto_injectdb.py
+++ auto_injectdb.py
@@ -5,7 +5,9 @@ import numpy as np
 import time
 import sys
 import random
+import MySQLdb
 from pandas import DataFrame
+from sqlalchemy import create_engine
 
 
 # Golbal Variable
@@ -15,6 +17,7 @@ DL_LIST='/opt/automateams/var/dl_list.csv'
 filename=sys.argv[1]
 ADEX_FILE='/opt/output/adexchange.csv'
 ZM_FILE='/opt/output/adzimbra.csv'
+IMPORT_DB='/opt/output/importdb.csv'
 
 # Index COS file
 cos_list = pd.read_csv(COS_LIST,sep=',|;', engine='python')
@@ -28,11 +31,13 @@ df = pd.read_csv(filename, usecols=['EMP_NO','USERNAME','FIRST_NAME','LAST_NAME'
 
 #---------- Join table 1 for zimbra------------------
 maindf = cos_list.merge(df, how = 'inner', on = ['COS'])
+print('main df print')
 print (maindf)
 
 ## Split email address to sAMAccountName and limit just 20 Char in there because of AD limitation
 sAMAccountName_field = maindf.EMAIL.str.split("@").str[0].str.slice(0, 20)
 maindf['sAMAccountName'] = sAMAccountName_field.str.slice(0, 20)
+print(sAMAccountName_field)
 
 ## Domain name
 domain_field = maindf.EMAIL.str.split("@").str[1]
@@ -61,6 +66,14 @@ maindf['telephoneNumber']='telephoneNumber'
 maindf['zimbraCOSid']='zimbraCOSid'
 maindf['alias']='aaa'
 maindf['adlm']='adlm'
+#maindf['status_employee']='status_employee'
+#maindf['status_email']='status_email'
+#maindf['id_lync_stts_ftime']='id_lync_stts_ftime'
+#maindf['id_mobile_stts_ftime']='id_mobile_stts_ftime'
+maindf['status_employee']='0'
+maindf['status_email']='0'
+maindf['id_lync_stts_ftime']='0'
+maindf['id_mobile_stts_ftime']='0'
 #--
 maindf['ZEMP_NO']="'"+maindf.EMP_NO.astype('str')+"'"
 maindf['alias_domain']=maindf['USERNAME']+'@'+maindf['domain_name']
@@ -72,6 +85,7 @@ maindf['Zphone']="'"+maindf.MOBILE_PHONE.astype('str')+"'"
 
 #---------- Join table 2 for zimbra DL------------------
 dl_df = dl_list2.merge(maindf, how = 'inner', on = ['COS','CITY'])
+print ('print dl_df')
 print (dl_df)
 
 # DECIDE Field by feature and Position
@@ -112,3 +126,35 @@ CONV_ZM_ONLY.to_csv(ZM_FILE,encoding='utf-8',sep=' ', index = None, header=None,
 
 # DL IN ZIMBRA
 dl_df.to_csv(ZM_FILE,encoding='utf-8',sep=' ', index = None, header=None, mode = 'a', columns=['adlm','dl_address','EMAIL'])
+
+#create_user_imprtdb = maindf[['EMP_NO','USERNAME','sAMAccountName','PASSWORD','FIRST_NAME','LAST_NAME','DisplayName','EMAIL','POSITION','COST_CENTER','WORK_LOCATION','status_employee','status_email','date_created','TICKET','id_lync_stts_ftime','id_mobile_stts_ftime','MOBILE_PHONE']]
+
+tbl_db=['nik','username','upn','password','f_name','l_name','d_name','email','position','department','office','status_employee','status_email','date_created','otrs_date_created','id_lync_stts','id_mobile_stts','mobile_phone']
+
+maindf.to_csv(IMPORT_DB,encoding='utf-8',sep=',',index = None, quotechar='"', header=tbl_db,columns=['EMP_NO','USERNAME','sAMAccountName','PASSWORD','FIRST_NAME','LAST_NAME','DisplayName','EMAIL','POSITION','COST_CENTER','WORK_LOCATION','status_employee','status_email','date_created','TICKET','id_lync_stts_ftime','id_mobile_stts_ftime','MOBILE_PHONE'])
+
+
+#------------------------------------- INSERT DB AMS
+#-- DB AMS
+engine = create_engine("mysql://root:paganini@localhost/homecredit")
+#Base.metadata.create_all(engine)
+
+insertdb = pd.read_csv(IMPORT_DB)
+insertdb.to_sql(con=engine, index=False, name='tbl_users', if_exists='replace')
+#insertdb.to_sql(con=engine, index=False, name='tbl_users', if_exists='append')
+#insertdb.to_sql()
+
+#ams = MySQLdb.connect(host= "localhost",
+#                  user="root",
+#                  passwd="paganini",
+#                  db="homecredit")
+
+#mycursor = ams.cursor()
+#create_employee = "INSERT INTO tbl_users (nik,username,upn,password,f_name,l_name,d_name,email,position,department,office,status_employee,status_email,date_created,otrs_date_created,id_lync_stts,id_mobile_stts,mobile_phone) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+#number_of_rows = mycursor.executemany(create_employee, printtest)
+
+#conn.commit()
+#db.close()
+
+#print(mycursor.rowcount, "record inserted.")
